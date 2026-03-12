6d215d6f9ad509b53969c32546dca7d55b1e9753
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-03-12 23:44:46 +0100

    fix methods in EnrollmentRepository

--- courseManagment/src/main/java/com/school/coursemanagment/repository/EnrollmentRepository.java
+++ courseManagment/src/main/java/com/school/coursemanagment/repository/EnrollmentRepository.java
@@ -1,8 +1,16 @@
 package com.school.coursemanagment.repository;
 
 import com.school.coursemanagment.model.Enrollment;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment,Long> {
 
+    List<Enrollment> findByUserIdUser(Long idUser);
+
+    List<Enrollment> findByCourseIdCourse(Long idCourse);
+
+    boolean existsByUserIdUserAndCourseIdCourse(Long idUser,Long idCourse);
 }
