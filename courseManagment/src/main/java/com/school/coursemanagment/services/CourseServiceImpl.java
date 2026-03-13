142033b1fa9bf9f9afd174f240d2d9ed36418933
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-03-13 16:42:54 +0100

    fix bug of creation course with verif instructor

--- courseManagment/src/main/java/com/school/coursemanagment/services/CourseServiceImpl.java
+++ courseManagment/src/main/java/com/school/coursemanagment/services/CourseServiceImpl.java
@@ -1,7 +1,10 @@
 package com.school.coursemanagment.services;
 
+import com.school.coursemanagment.Enum.Role;
 import com.school.coursemanagment.model.Course;
+import com.school.coursemanagment.model.User;
 import com.school.coursemanagment.repository.CourseRepository;
+import com.school.coursemanagment.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +16,19 @@ public class CourseServiceImpl implements CourseService{
     @Autowired
     CourseRepository courseRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public Course saveCourse(Course course) {
+
+        Long idUser = course.getCreator().getIdUser();
+        User user = userRepository.getReferenceById(idUser);
+
+        if(!user.getRole().toString().equalsIgnoreCase(String.valueOf(Role.instructor))){
+            throw new RuntimeException("Only Instructor can create courses");
+        }
+        course.setCreator(user);
         return courseRepository.save(course);
     }
 
