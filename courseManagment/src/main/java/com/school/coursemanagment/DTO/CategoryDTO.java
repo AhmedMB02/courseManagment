0dbaa729f0e7141df8b9ad97d8e3c9607f8675b8
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-03-15 21:55:03 +0100

    add CategoryDTO

---
+++ courseManagment/src/main/java/com/school/coursemanagment/DTO/CategoryDTO.java
@@ -0,0 +1,20 @@
+package com.school.coursemanagment.DTO;
+
+import com.school.coursemanagment.model.Course;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryDTO {
+    private Long idCategory;
+    private String name;
+    private String description;
+    private List<Course> courses;
+}
