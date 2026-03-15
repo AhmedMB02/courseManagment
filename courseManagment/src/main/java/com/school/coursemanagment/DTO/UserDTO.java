9a2e490982c22e1b6e2803febac143303d51dd57
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-03-15 22:46:19 +0100

    add UserDTO

--- courseManagment/src/main/java/com/school/coursemanagment/DTO/UserDTO.java
+++ courseManagment/src/main/java/com/school/coursemanagment/DTO/UserDTO.java
@@ -1,5 +1,27 @@
 package com.school.coursemanagment.DTO;
 
 
+
+import com.school.coursemanagment.Enum.Role;
+import com.school.coursemanagment.model.Course;
+import com.school.coursemanagment.model.Enrollment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserDTO {
+    private Long idUser;
+    private String name;
+    private String email;
+    private Role role;
+
+    private List<Course> courses;
+    private List<Enrollment> enrollments;
 }
