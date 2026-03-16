85bd4c8f54a4c81167698095999c0967ce0efd0a
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-03-16 01:59:05 +0100

    add DTO to CategoryService : not finished

--- courseManagment/src/main/java/com/school/coursemanagment/services/CategoryService.java
+++ courseManagment/src/main/java/com/school/coursemanagment/services/CategoryService.java
@@ -1,15 +1,19 @@
 package com.school.coursemanagment.services;
 
+import com.school.coursemanagment.DTO.CategoryDTO;
+import com.school.coursemanagment.DTO.CourseDTO;
 import com.school.coursemanagment.model.Category;
 import com.school.coursemanagment.model.Course;
 
 import java.util.List;
 
 public interface CategoryService {
-    Category saveCategory(Category category);
-    Category updateCategory(Category category);
+    CategoryDTO saveCategory(Category category);
+    CategoryDTO updateCategory(Category category);
     void deleteCategoryById(Long id);
-    List<Category> getAllCategory();
-    Category getCategoryById(Long id);
-    List<Course> getCourseByIdCategory(Long id);
+    List<CategoryDTO> getAllCategory();
+    CategoryDTO getCategoryById(Long id);
+    List<CategoryDTO> getCourseByIdCategory(Long id);
+    CategoryDTO convertEntityToDto(Category category);
+    Category convertDtoToEntity(CategoryDTO categoryDTO);
 }
--- courseManagment/src/main/java/com/school/coursemanagment/services/CategoryServiceImpl.java
+++ courseManagment/src/main/java/com/school/coursemanagment/services/CategoryServiceImpl.java
@@ -1,5 +1,7 @@
 package com.school.coursemanagment.services;
 
+import com.school.coursemanagment.DTO.CategoryDTO;
+import com.school.coursemanagment.DTO.CourseDTO;
 import com.school.coursemanagment.model.Category;
 import com.school.coursemanagment.model.Course;
 import com.school.coursemanagment.repository.CategoryRepository;
@@ -7,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CategoryServiceImpl implements CategoryService{
 
@@ -14,13 +18,14 @@ public class CategoryServiceImpl implements CategoryService{
     CategoryRepository categoryRepository;
 
     @Override
-    public Category saveCategory(Category category) {
-        return categoryRepository.save(category);
+    public CategoryDTO saveCategory(Category category) {
+
+        return convertEntityToDto(categoryRepository.save(category));
     }
 
     @Override
-    public Category updateCategory(Category category) {
-        return categoryRepository.save(category);
+    public CategoryDTO updateCategory(Category category) {
+        return convertEntityToDto(categoryRepository.save(category));
     }
 
     @Override
@@ -29,17 +34,38 @@ public class CategoryServiceImpl implements CategoryService{
     }
 
     @Override
-    public List<Category> getAllCategory() {
-        return categoryRepository.findAll();
+    public List<CategoryDTO> getAllCategory() {
+
+        return categoryRepository.findAll().stream()
+                .map(this::convertEntityToDto)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Category getCategoryById(Long id) {
-        return categoryRepository.getReferenceById(id);
+    public CategoryDTO getCategoryById(Long id) {
+        return convertEntityToDto(categoryRepository.getReferenceById(id));
     }
 
     @Override
-    public List<Course> getCourseByIdCategory(Long id) {
-        return categoryRepository.findByIdCategory(id);
+    public List<CourseDTO> getCourseByIdCategory(Long id) {
+        return categoryRepository.findByIdCategory(id).stream()
+                .map(this::convertEntityToDto)
+                .collect(Collectors.toList());
     }
+
+    @Override
+    public CategoryDTO convertEntityToDto(Category category) {
+        return CategoryDTO.builder()
+                .idCategory(category.getIdCategory())
+                .name(category.getName())
+                .description(category.getDescription())
+                .build();
+    }
+
+    @Override
+    public Category convertDtoToEntity(CategoryDTO categoryDTO) {
+        return null;
+    }
+
+
 }
