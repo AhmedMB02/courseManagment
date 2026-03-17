756a8d66782bf225a9c7579e189ed1d39f7cba11
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-03-17 21:42:44 +0100

    add EnrollmentDTO

---
+++ courseManagment/src/main/java/com/school/coursemanagment/DTO/EnrollmentDTO.java
@@ -0,0 +1,24 @@
+package com.school.coursemanagment.DTO;
+
+import com.school.coursemanagment.model.Course;
+import com.school.coursemanagment.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EnrollmentDTO {
+    private Long id;
+
+    private LocalDate enrollmentDate;
+
+    private User user;
+
+    private Course course;
+}
