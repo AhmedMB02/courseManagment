ed30f9ec6be4d63cee43ca35e0b7f0bbaea1c353
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-03-22 02:07:28 +0100

    fix Enrollment APIs in controller

--- courseManagment/src/main/java/com/school/coursemanagment/controller/EnrollmentController.java
+++ courseManagment/src/main/java/com/school/coursemanagment/controller/EnrollmentController.java
@@ -1,5 +1,6 @@
 package com.school.coursemanagment.controller;
 
+import com.school.coursemanagment.DTO.EnrollmentDTO;
 import com.school.coursemanagment.model.Enrollment;
 import com.school.coursemanagment.services.EnrollmentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,22 +16,22 @@ public class EnrollmentController {
     EnrollmentService enrollmentService;
 
     @PostMapping("/enroll")
-    public Enrollment createEnrollment(@RequestBody Enrollment enrollment){
-        return enrollmentService.enrollStudent(enrollment);
+    public EnrollmentDTO createEnrollment(@RequestBody EnrollmentDTO enrollmentDTO){
+        return enrollmentService.enrollStudent(enrollmentDTO);
     }
 
     @GetMapping("/all")
-    public List<Enrollment> getAllEnrollments(){
+    public List<EnrollmentDTO> getAllEnrollments(){
         return enrollmentService.getAllEnrollments();
     }
 
     @GetMapping("student/{id}")
-    public List<Enrollment> getAllEnrollmentsByStudent(@PathVariable Long id){
+    public List<EnrollmentDTO> getAllEnrollmentsByStudent(@PathVariable Long id){
         return enrollmentService.getEnrollmentsByStudent(id);
     }
 
     @GetMapping("/all/course/{id}")
-    public List<Enrollment> getEnrollmentsByCourse(@PathVariable Long idCourse){
+    public List<EnrollmentDTO> getEnrollmentsByCourse(@PathVariable Long idCourse){
         return enrollmentService.getEnrollmentsByCourse(idCourse);
     }
 
@@ -40,7 +41,7 @@ public class EnrollmentController {
     }
 
     @GetMapping("/{id}")
-    public Enrollment getEnrollmentById(@PathVariable Long idEnrollment){
+    public EnrollmentDTO getEnrollmentById(@PathVariable Long idEnrollment){
         return enrollmentService.getEnrollmentById(idEnrollment);
     }
 }
