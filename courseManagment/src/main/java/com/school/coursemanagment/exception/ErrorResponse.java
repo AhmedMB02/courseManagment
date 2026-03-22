7c227a929f492637e47203ac01de0de97873fe0d
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-03-23 00:27:03 +0100

    add ErrorResponse : exception

---
+++ courseManagment/src/main/java/com/school/coursemanagment/exception/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.school.coursemanagment.exception;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime localDateTime;
+    private int status;
+    private String error;
+    private String message;
+}
