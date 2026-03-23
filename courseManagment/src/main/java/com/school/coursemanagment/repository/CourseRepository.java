153964b3be264684baf943e85dc0a27606f00316
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-03-24 00:48:56 +0100

    add method findByCategoryIdCategory in CourseRepository

--- courseManagment/src/main/java/com/school/coursemanagment/repository/CourseRepository.java
+++ courseManagment/src/main/java/com/school/coursemanagment/repository/CourseRepository.java
@@ -3,5 +3,9 @@ package com.school.coursemanagment.repository;
 import com.school.coursemanagment.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course,Long> {
+
+    List<Course> findByCategoryIdCategory(Long idCategory);
 }
