89eeae2649114ed6f0a200c84c17497e0b3395f8
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-03-24 01:22:25 +0100

    add method existsByName in CategoryRepository

--- courseManagment/src/main/java/com/school/coursemanagment/repository/CategoryRepository.java
+++ courseManagment/src/main/java/com/school/coursemanagment/repository/CategoryRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category,Long> {
-    List<Course> findByIdCategory(Long idCategory);
+    boolean existsByName(String name);
 }
