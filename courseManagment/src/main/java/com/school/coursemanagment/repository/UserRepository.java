d8db3e6092b3f159c4b5aace74edc173259dfd79
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-03-25 01:42:06 +0100

    add method existsByEmail in UserRepository

--- courseManagment/src/main/java/com/school/coursemanagment/repository/UserRepository.java
+++ courseManagment/src/main/java/com/school/coursemanagment/repository/UserRepository.java
@@ -4,11 +4,12 @@ package com.school.coursemanagment.repository;
 import com.school.coursemanagment.Enum.Role;
 import com.school.coursemanagment.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
     List<User> findByRole(Role role);
+
+    boolean existsByEmail(String email);
 }
