322fbe722af30e490866094c50270ae857de0ed7
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-04-02 00:56:02 +0100

    fix UserDTO with validations

--- courseManagment/src/main/java/com/school/coursemanagment/DTO/UserDTO.java
+++ courseManagment/src/main/java/com/school/coursemanagment/DTO/UserDTO.java
@@ -5,11 +5,13 @@ package com.school.coursemanagment.DTO;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.school.coursemanagment.Enum.Role;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.processing.Pattern;
 
 
 @Data
@@ -23,5 +25,10 @@ public class UserDTO {
     private Role role;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 100)
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)(?=.*[@$!%*?&]).+$"
+    )
     private String password;
 }
