93828dc99fcb5f6ecdd0bedf4375016efe345b14
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-04-02 00:05:36 +0100

    add annotations

--- courseManagment/src/main/java/com/school/coursemanagment/config/SecurityConfig.java
+++ courseManagment/src/main/java/com/school/coursemanagment/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.school.coursemanagment.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -9,6 +11,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
