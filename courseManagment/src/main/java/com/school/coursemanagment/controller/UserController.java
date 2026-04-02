a0af35931d106d4b898570bba4af1a924561dbc1
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-04-02 01:00:34 +0100

    some changes UserController

--- courseManagment/src/main/java/com/school/coursemanagment/controller/UserController.java
+++ courseManagment/src/main/java/com/school/coursemanagment/controller/UserController.java
@@ -4,7 +4,9 @@ import com.school.coursemanagment.DTO.UserDTO;
 import com.school.coursemanagment.model.User;
 import com.school.coursemanagment.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,21 +24,23 @@ public class UserController {
     }
 
     @PutMapping("/updateuser/{id}")
-    public UserDTO updateUser(@PathVariable Long id,@RequestBody UserDTO userDTO){
+    public UserDTO updateUser(@PathVariable Long id,@Validated @RequestBody UserDTO userDTO){
         userDTO.setIdUser(id);
         return userService.updateUser(userDTO);
     }
 
     @DeleteMapping("/delete/{id}")
-    @PreAuthorize("hasRole('admin')")
+    @PreAuthorize("hasRole('ADMIN')")
     public void deleteUser(@PathVariable Long id){userService.deleteUserById(id);}
 
     @GetMapping("/allUsers")
+    @PreAuthorize("hasRole('ADMIN')")
     public List<UserDTO>getAllUsers(){
         return userService.getAllUsers();
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public UserDTO getUserById(@PathVariable Long id){
         return userService.getUserById(id);
     }
@@ -48,5 +52,19 @@ public class UserController {
     @GetMapping("/instructors")
     @PreAuthorize("hasAnyRole('admin','instructor')")
     public List<UserDTO> getAllInstructor(){return userService.getAllInstructor(); }
+
+
+    /*
+    @PostMapping("/login")
+    public ResponseEntity<?>login(@Validated  ){
+
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDTO> getMe(@AuthenticationPrincipal User currentUser){
+
+    }
+*/
+
 
 }
