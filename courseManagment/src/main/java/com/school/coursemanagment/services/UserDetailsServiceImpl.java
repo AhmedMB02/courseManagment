bc14dc1773d9af5fba278b551a24ba3bffb48ebf
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-04-04 17:24:20 +0100

    add UserDetailsServiceImpl

---
+++ courseManagment/src/main/java/com/school/coursemanagment/services/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.school.coursemanagment.services;
+
+import com.school.coursemanagment.exception.ResourceNotFoundException;
+import com.school.coursemanagment.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User Not found"+email));
+    }
+}
