171a9f6d66ea100fdb36a8fe5f3f5353321151e5
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-04-09 21:55:57 +0100

    change annotation : add Valid

--- courseManagment/src/main/java/com/school/coursemanagment/controller/UserController.java
+++ courseManagment/src/main/java/com/school/coursemanagment/controller/UserController.java
@@ -3,6 +3,7 @@ package com.school.coursemanagment.controller;
 import com.school.coursemanagment.DTO.UserDTO;
 import com.school.coursemanagment.model.User;
 import com.school.coursemanagment.services.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,17 +18,17 @@ import java.util.List;
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-    @Autowired
-    UserService userService;
+
+    private final UserService userService;
 
 
     @PostMapping("/adduser")
-    public UserDTO createUser(@Validated @RequestBody UserDTO userDTO){
+    public UserDTO createUser(@Valid @RequestBody UserDTO userDTO){
         return userService.saveUser(userDTO);
     }
 
     @PutMapping("/updateuser/{id}")
-    public UserDTO updateUser(@PathVariable Long id,@Validated @RequestBody UserDTO userDTO){
+    public UserDTO updateUser(@PathVariable Long id,@Valid @RequestBody UserDTO userDTO){
         userDTO.setIdUser(id);
         return userService.updateUser(userDTO);
     }
