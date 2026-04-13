37f332f254f91ebc2c1a4dd32760b858847cdfeb
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-04-13 22:30:19 +0100

    authResponse : add accessToken

--- courseManagment/src/main/java/com/school/coursemanagment/DTO/AuthResponse.java
+++ courseManagment/src/main/java/com/school/coursemanagment/DTO/AuthResponse.java
@@ -1,5 +1,6 @@
 package com.school.coursemanagment.DTO;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,4 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
 }
