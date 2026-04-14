5fc21e1ed0cb090d4a1d46cc0f427a5141c1c40d
AhmedMB02 <ahmedmbarek16@gmail.com>
2026-04-14 23:30:03 +0100

    change

--- courseManagment/src/main/java/com/school/coursemanagment/security/JwtAuthenticationFilter.java
+++ courseManagment/src/main/java/com/school/coursemanagment/security/JwtAuthenticationFilter.java
@@ -10,7 +10,8 @@ import java.io.IOException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
 
     }
 }
